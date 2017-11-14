a826d7934abeae3a97abeeb17f4435168a8710d2
tkwon <tkwon@gracenote.com>
2017-11-14 17:52:16 +0900

    first commit

---
+++ src/hello.js
@@ -0,0 +1,34 @@
+import {PanelCtrl} from 'app/plugins/sdk';
+
+export class HelloCtrl extends PanelCtrl {
+  constructor($scope, $injector) {
+    super($scope, $injector);
+    _.defaultsDeep(this.panel, panelDefaults);
+
+    this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
+    this.events.on('panel-teardown', this.onPanelTeardown.bind(this));
+    this.events.on('panel-initialized', this.render.bind(this));
+  }
+
+  onInitEditMode() {
+		console.log('entering edit mode');
+  }
+
+  onPanelTeardown() {
+		console.log('teared down');
+  }
+
+  link(scope, elem) {
+    this.events.on('render', () => {
+      const $panelContainer = elem.find('.panel-container');
+
+      if (this.panel.bgColor) {
+        $panelContainer.css('background-color', this.panel.bgColor);
+      } else {
+        $panelContainer.css('background-color', '');
+      }
+    });
+  }
+}
+
+HelloCtrl.templateUrl = 'module.html';
